17b7bbdf6d5787c31929fd611c185d42d9e06359
Léo Pradel <pradel.leo@gmail.com>
2026-05-16 19:10:40 +0200

    feat: setup monorepo (#1)

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    options: { typeAware: true, typeCheck: true },
+  },
+  run: {
+    cache: true,
+  },
+});
