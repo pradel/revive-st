0db492fed014df8a7a41ca06b3e13f369c4e5c18
Léo Pradel <pradel.leo@gmail.com>
2026-05-22 20:05:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -79,7 +79,6 @@ export default defineConfig({
       "consistent-type-definitions": "off",
       "no-explicit-any": "off",
       "no-non-null-assertion": "off",
-      "unified-signatures": "off",
       "consistent-type-imports": "off",
     },
     overrides: [
