18c171c49203e4fda1ed12fcbc14bff191cad886
Léo Pradel <pradel.leo@gmail.com>
2026-05-22 22:00:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -59,13 +59,11 @@ export default defineConfig({
       "typescript/explicit-member-accessibility": "off",
       "typescript/explicit-module-boundary-types": "off",
       "typescript/no-unnecessary-condition": "off",
+      "typescript/no-unsafe-type-assertion": "off",
       "typescript/prefer-readonly-parameter-types": "off",
       "typescript/strict-boolean-expressions": "off",
       "typescript/strict-void-return": "off",
       "typescript/unified-signatures": "off",
-
-      // TMP
-      "no-unsafe-type-assertion": "off",
     },
     overrides: [
       {
