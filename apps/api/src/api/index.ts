9c38faa513d76ccc7b0c120c916acd9298a31e29
Léo Pradel <pradel.leo@gmail.com>
2026-05-27 15:08:15 +0200

    feat: create api server

---
+++ apps/api/src/api/index.ts
@@ -0,0 +1,3 @@
+import { defineHandler } from "nitro";
+
+export default defineHandler(() => ({ message: "Hello Nitro!" }));
---
+++ apps/api/vite.config.ts
@@ -0,0 +1,6 @@
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  plugins: [nitro()],
+});
